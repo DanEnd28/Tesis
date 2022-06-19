908f59cf9c0f90f5aa6b38e9d48dc3e9bdfdf657
DanEnd28 <dannyendara28@gmail.com>
2022-06-19 18:45:55 -0400

    vamos a llorar con esto

--- Dashboard/index.php
+++ Dashboard/index.php
@@ -1,103 +1,123 @@
 <?php
 
+require "../Model/conexion.php";
+
+if($_POST){
+    $usuario=$_POST['usuario'];
+    $password=$_POST['password'];
+
+    $sql = "Select Cedula.usuario, Usuario.usuario, Contraseña.usuario, Rol_id.usuario_rol, Desc_rol.rol from usuario inner join usuario_rol on Cedula.usuario=Cedula.usuario_rol inner join rol on Id_rol.usuario_rol=Rol_id.rol where Usuario.usuario='$usuario'";
+
+    $resultado = $mysqli->query($sql);
+    $num = $resultado->num_rows;
+
+    if($num>0){
+
+        $row= $resultado->fetch_assoc();
+        $password_bd = $row['Contraseña.usuario'];
+
+        $pass_c=sha1($password);
+
+        if ($password_bd=$pass_c) {
+
+            $_SESSION['Cedula']=$row['Cedula.usuario'];
+            $_SESSION['Rol']=$row['Rol_id.usuario_rol'];
+        
+            //header{"Location: principal.php"};
+        
+        } else {
+            echo "La Contraseña no Coincide";
+        }
+
+    }else{
+        echo "No Existe Usuario";
+    }
+
+}
 
 ?>
 
     <!DOCTYPE html>
     <html lang="en">
-
     <head>
-
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
         <meta name="description" content="">
         <meta name="author" content="">
-
-        <title>SB Admin 2 - Login</title>
-
+        <title>Inicio de Sesion</title>
         <!-- Custom fonts for this template-->
         <link href="vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
-
+        <link href="../Librerias/googlefonts.css" rel="stylesheet">
+        <link rel="stylesheet" href="../Librerias/estilos.css">
         <!-- Custom styles for this template-->
         <link href="css/sb-admin-2.min.css" rel="stylesheet">
-
     </head>
-
     <body class="bg-gradient-primary">
-
         <div class="container">
-
             <!-- Outer Row -->
             <div class="row justify-content-center">
-
                 <div class="col-xl-10 col-lg-12 col-md-9">
-
                     <div class="card o-hidden border-0 shadow-lg my-5">
                         <div class="card-body p-0">
                             <!-- Nested Row within Card Body -->
                             <div class="row">
-                                <div class="col-lg-6 d-none d-lg-block bg-login-image"></div>
+                                <div class="col-lg-6 d-none d-lg-block"><img src="../Imagenes/login.jpg" alt="Emapanada" class="emplog"></div>
                                 <div class="col-lg-6">
                                     <div class="p-5">
                                         <div class="text-center">
-                                            <h1 class="h4 text-gray-900 mb-4">Welcome Back!</h1>
+                                            <h1 class="h4 text-gray-900 mb-4">Bienvenido</h1>
                                         </div>
-                                        <form class="user">
+                                        <form class="user" method="POST" action="<?php echo $_SERVER['PHP_SELF']; ?>">
                                             <div class="form-group">
-                                                <input type="email" class="form-control form-control-user" id="exampleInputEmail" aria-describedby="emailHelp" placeholder="Enter Email Address...">
+                                                <input type="text" class="form-control form-control-user" id="Usuario" aria-describedby="Usuario" placeholder="Introduzca Usuario..." name="usuario">
                                             </div>
                                             <div class="form-group">
-                                                <input type="password" class="form-control form-control-user" id="exampleInputPassword" placeholder="Password">
+                                                <input type="password" class="form-control form-control-user" id="exampleInputPassword" placeholder="Contraseña" name="password">
+                                            </div><div class="form-group">
+                                                <div class="custom-control custom-checkbox small">
+                                                    <input type="checkbox" class="custom-control-input" id="customCheck">
+                                                    <label class="custom-control-label" for="customCheck">Mostrar Contraseña</label>
+                                                </div>
                                             </div>
                                             <div class="form-group">
                                                 <div class="custom-control custom-checkbox small">
                                                     <input type="checkbox" class="custom-control-input" id="customCheck">
-                                                    <label class="custom-control-label" for="customCheck">Remember
-                                                    Me</label>
+                                                    <label class="custom-control-label" for="customCheck">Recuerdame</label>
                                                 </div>
                                             </div>
                                             <a href="index.html" class="btn btn-primary btn-user btn-block">
-                                            Login
+                                            Iniciar Sesion
                                         </a>
                                             <hr>
                                             <a href="index.html" class="btn btn-google btn-user btn-block">
-                                                <i class="fab fa-google fa-fw"></i> Login with Google
+                                                <i class="fab fa-google fa-fw"></i> Iniciar Sesion con Google
                                             </a>
                                             <a href="index.html" class="btn btn-facebook btn-user btn-block">
-                                                <i class="fab fa-facebook-f fa-fw"></i> Login with Facebook
+                                                <i class="fab fa-facebook-f fa-fw"></i> Iniciar Sesion con Facebook
                                             </a>
                                         </form>
                                         <hr>
                                         <div class="text-center">
-                                            <a class="small" href="forgot-password.html">Forgot Password?</a>
+                                            <a class="small" href="forgot-password.html">Olvide mi Contraseña</a>
                                         </div>
                                         <div class="text-center">
-                                            <a class="small" href="register.html">Create an Account!</a>
+                                            <a class="small" href="register.html">Registrate!</a>
                                         </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
                     </div>
-
                 </div>
-
             </div>
-
         </div>
-
         <!-- Bootstrap core JavaScript-->
         <script src="vendor/jquery/jquery.min.js"></script>
         <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-
         <!-- Core plugin JavaScript-->
         <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
-
         <!-- Custom scripts for all pages-->
         <script src="js/sb-admin-2.min.js"></script>
-
     </body>
-
     </html>
