5935bc90b03db693eda87f5e4cd8382fb70d427f
DanEnd28 <dannyendara28@gmail.com>
2022-07-13 23:58:32 -0400

    a

--- Nuevo/signin.php
+++ Nuevo/signin.php
@@ -53,29 +53,30 @@
                                 <img class="logitologin d-inline-flex" src="Imagenes/Empanada.png" alt="Emp">
                             </a>
                         </div>
-                        <div class="form-floating mb-3">
-                            <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
-                            <label for="floatingInput">Usuario</label>
-                        </div>
-                        <div class="form-floating mb-4">
-                            <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
-                            <label for="floatingPassword">Contraseña</label>
-                        </div>
-                        <div class="d-flex align-items-center justify-content-between mb-4">
-                            <div class="form-check">
-                                <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                                <label class="form-check-label" for="exampleCheck1">Mostrar Contraseña</label>
+                        <form action="index.php" method="POST">
+                            <div class="form-floating mb-3">
+                                <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+                                <label for="floatingInput">Usuario</label>
                             </div>
-
-                        </div>
-                        <button type="submit" class="btn btn-primary py-3 w-100 mb-4">Sign In</button>
-                        <p class="text-center mb-0">Olvido la Contraseña? <a href="">Recuperar</a></p>
-                        <p class="text-center mb-0">No tiene Cuenta? <a href="">Registrarse</a></p>
+                            <div class="form-floating mb-4">
+                                <input type="password" class="form-control" id="floatingPassword" placeholder="Password">
+                                <label for="floatingPassword">Contraseña</label>
+                            </div>
+                            <div class="d-flex align-items-center justify-content-between mb-4">
+                                <div class="form-check">
+                                    <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                                    <label class="form-check-label" for="exampleCheck1">Mostrar Contraseña</label>
+                                </div>
+                        </form>
                     </div>
+                    <button type="submit" class="btn btn-primary py-3 w-100 mb-4">Ingresar</button>
+                    <p class="text-center mb-0">Olvido la Contraseña? <a href="">Recuperar</a></p>
+                    <p class="text-center mb-0">No tiene Cuenta? <a href="">Registrarse</a></p>
                 </div>
             </div>
         </div>
-        <!-- Sign In End -->
+    </div>
+    <!-- Sign In End -->
     </div>
 
     <!-- JavaScript Libraries -->
